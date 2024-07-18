e784ce3bbdea763ca174fa45beaa29837669d405
Yaroslav_MBP <59771733+keyoneinc@users.noreply.github.com>
2024-07-18 15:20:52 +0100

    created SubscriberFee class, added definition

---
+++ SubscriberFee.cpp
@@ -0,0 +1,79 @@
+//
+// Created by Yaroslav! on 18/07/2024.
+//
+
+#include "SubscriberFee.h"
+
+SubscriberFee::SubscriberFee(string lastName, string firstName, string middleName, string address, string city,
+                             string passportData) {
+    this->lastName = lastName;
+    this->firstName = firstName;
+    this->middleName = middleName;
+    this->address = address;
+    this->city = city;
+    this->passportData = passportData;
+
+}
+
+SubscriberFee::~SubscriberFee() {
+
+}
+
+void SubscriberFee::setLastName(string lastName) {
+    this->lastName = lastName;
+}
+
+void SubscriberFee::setFirstName(string firstName) {
+    this->firstName = firstName;
+}
+
+void SubscriberFee::setMiddleName(string middleName) {
+    this->middleName = middleName;
+}
+
+void SubscriberFee::setAddress(string address) {
+    this->address = address;
+}
+
+void SubscriberFee::setCity(string city) {
+    this->city = city;
+}
+
+void SubscriberFee::setPassportData(string passportData) {
+    this->passportData = passportData;
+}
+
+string SubscriberFee::getLastName() const {
+    return lastName;
+}
+
+string SubscriberFee::getFirstName() const {
+    return firstName;
+}
+
+string SubscriberFee::getMiddleName() const {
+    return middleName;
+}
+
+string SubscriberFee::getAddress() const {
+    return address;
+}
+
+string SubscriberFee::getCity() const {
+    return city;
+}
+
+string SubscriberFee::getPassportData() const {
+    return passportData;
+}
+
+void SubscriberFee::displayInfo() const {
+    cout << "Subscriber: " << lastName << " " << firstName << " " << middleName << endl;
+    cout << "Address: " << address << ", " << city << endl;
+    cout << "Passport Data: " << passportData << endl;
+
+}
+
+
+
+
---
+++ SubscriberFee.h
@@ -0,0 +1,37 @@
+//
+// Created by Yaroslav! on 18/07/2024.
+//
+
+#include "iostream"
+#include "string"
+using namespace std;
+
+class SubscriberFee {
+protected:
+    string lastName;
+    string firstName;
+    string middleName;
+    string address;
+    string city;
+    string passportData;
+public:
+    SubscriberFee(string lastName, string firstName, string middleName, string address, string city, string passportData);
+    virtual ~SubscriberFee();
+
+    void setLastName(string lastName);
+    void setFirstName(string firstName);
+    void setMiddleName(string middleName);
+    void setAddress(string address);
+    void setCity(string city);
+    void setPassportData(string passportData);
+
+    string getLastName() const;
+    string getFirstName() const;
+    string getMiddleName() const;
+    string getAddress() const;
+    string getCity() const;
+    string getPassportData() const;
+
+    virtual void displayInfo() const;
+
+};
